f3959b4c0b415a3c6b67bfcb643b0fb03f152fd4
Hércules HRsniper <63937242+HRsniper@users.noreply.github.com>
2020-11-19 14:19:39 -0400

    refactor: simplify get and sum time logic, added ; we were missing.
    Co-Authored-By: Raphael de Oliveira  <raphaeldeoliveiramoura@gmail.com>

--- src/crawler.js
+++ src/crawler.js
@@ -6,7 +6,7 @@ import { getVideosTime } from './getVideosTime.js';
 const executablePaths = {
   'linux': '/usr/bin/google-chrome',
   'darwin': '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-  'win32':  'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'
+  'win32': 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'
 };
 
 const platform = os.platform();
@@ -19,29 +19,19 @@ const platform = os.platform();
 
   const page = await browser.newPage();
 
-  const cdDiegao = await getVideosTime(page, 'https://www.youtube.com/playlist?list=PL85ITvJ7FLohhULgUFkYBf2xcXCG6yfVV')
-  const cdMaykao = await getVideosTime(page, 'https://www.youtube.com/playlist?list=PL85ITvJ7FLoifcDIBeuuAhh4_799RZaSc')
+  const cdDiegao = await getVideosTime(page, 'https://www.youtube.com/playlist?list=PL85ITvJ7FLohhULgUFkYBf2xcXCG6yfVV');
+  const cdMaykao = await getVideosTime(page, 'https://www.youtube.com/playlist?list=PL85ITvJ7FLoifcDIBeuuAhh4_799RZaSc');
+  // const PodcastUseCase = await getVideosTime(page, 'https://www.youtube.com/playlist?list=PL85ITvJ7FLoiOC_CjhpYjobA2tnt27TQc')
 
-  // let [hours, minutes, seconds] = await getVideosTime(page, 'https://www.youtube.com/playlist?list=PL85ITvJ7FLoiOC_CjhpYjobA2tnt27TQc')
-  let hours = cdDiegao[0] + cdMaykao[0];
-  let minutes = cdDiegao[1] + cdMaykao[1];
-  let seconds = cdDiegao[2] + cdMaykao[2];
+  // formatar tempo da maneira correta
+  const oneHour = 3600;
+  const fullTime = cdDiegao + cdMaykao;
 
-  // formatar da maneira correta
+  const hours = Math.floor(fullTime / oneHour);
+  const minutes = Math.floor((fullTime % oneHour) / 60);
+  const seconds = fullTime % 60;
 
-  const minutesFromSeconds = Math.floor(seconds / 60);
-
-  seconds = seconds % 60;
-  minutes += minutesFromSeconds;
-
-  const hoursFromMinutes = Math.floor(minutes / 60);
-
-  minutes = minutes % 60;
-  hours += hoursFromMinutes;
-
-  console.log(
-    `Produzimos ${hours}:${minutes}:${seconds} de conteúdo para o Code/Drops`
-  );
+  console.log(`Produzimos ${hours}:${minutes < 10 ? ('0' + minutes) : minutes}:${seconds} de conteúdo para o Code/Drops`);
 
   await browser.close();
 })();
--- src/getVideosTime.js
+++ src/getVideosTime.js
@@ -6,26 +6,26 @@ export async function getVideosTime(page, url) {
     waitUntil: 'networkidle0'
   });
 
-  const [hours, minutes, seconds] = await page.evaluate((body) => {
-    const videos = document.querySelectorAll('.ytd-playlist-video-list-renderer span.ytd-thumbnail-overlay-time-status-renderer')
+  return await page.evaluate((body) => {
+    const videos = document.querySelectorAll('.ytd-playlist-video-list-renderer span.ytd-thumbnail-overlay-time-status-renderer');
 
     let hours = 0, minutes = 0, seconds = 0;
 
-    videos.forEach(video => {
-      const fullTime = video.innerHTML.split(':')
+    videos.forEach((video) => {
+      const fullTime = video.innerHTML.split(':');
 
       if (fullTime.length > 2) {
-        hours += Number(fullTime[0])
-        minutes += Number(fullTime[1])
-        seconds += Number(fullTime[2])
+        hours += Number(fullTime[0]);
+        minutes += Number(fullTime[1]);
+        seconds += Number(fullTime[2]);
       } else {
-        minutes += Number(fullTime[0])
-        seconds += Number(fullTime[1])
+        minutes += Number(fullTime[0]);
+        seconds += Number(fullTime[1]);
       }
-    })
+    });
 
-    return [hours, minutes, seconds]
+    const fullTimeInSeconds = hours * 60 * 60 + minutes * 60 + seconds;
+
+    return fullTimeInSeconds;
   });
-
-  return [hours, minutes, seconds];
 }
